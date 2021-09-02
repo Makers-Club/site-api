b82f6531b91dbb3ba7e048a466b00edd1671a813
scan3ls <scan3ls@gmail.com>
2021-09-02 22:56:46 +0000

    Add notifications model to database

---
+++ models/notification.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Boolean
+from models.base import declarative_base, Base
+
+class Notification(Base, declarative_base):
+    __tablename__ = 'notifications'
+    id = Column(String(128), primary_key=True)
+    user_id = Column(String(128), nullable=False)
+    msg = Column(String(256), nullable=False)
+    is_read = Column(Boolean, default=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.id = kwargs.get('id') # or str(uuid4())
+        self.user_id = kwargs.get('user_id') # or str(uuid4())
+        self.msg = kwargs.get('msg')
+        self.is_read = kwargs.get('is_read')
